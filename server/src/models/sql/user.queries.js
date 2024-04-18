583801d600c08712641bd779fcd2e7021ac25362
Thomas P <thomas.philipps@gmail.com>
2024-04-19 00:08:04 +0200

    Models SQL queries

---
+++ server/src/models/sql/user.queries.js
@@ -0,0 +1,12 @@
+const userReadAllQuery = () => {
+  return `SELECT userId, userEmail, userName, userRole FROM Users`;
+};
+
+const userReadByIdQuery = (id) => {
+  return `SELECT userId, userEmail, userName, userRole FROM Users WHERE userId = ${id}`;
+};
+
+module.exports = {
+  userReadAllQuery,
+  userReadByIdQuery,
+};
