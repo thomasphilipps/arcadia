8a4e12e50652550a1dab502763655b3ee4024e1c
Thomas P <thomas.philipps@gmail.com>
2024-04-27 01:02:55 +0200

    Schedule admin implemented - styling to be added later

--- client/src/app/components/admin/partials/schedule-admin/schedule-admin.component.ts
+++ client/src/app/components/admin/partials/schedule-admin/schedule-admin.component.ts
@@ -1,21 +1,27 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { DataSource } from '@angular/cdk/collections';
 import { MatTableModule } from '@angular/material/table';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { ReactiveFormsModule, FormGroup, FormBuilder, FormControl } from '@angular/forms';
 
 import { Schedule } from '@interfaces/schedule.interface';
 import { ScheduleService } from '@services/schedule.service';
 import { CustomValidators } from '@validators/custom.validators';
 import { MatIconModule } from '@angular/material/icon';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
 
 @Component({
   selector: 'arz-schedule-admin',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, MatTableModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatIconModule,
+    NgxMaterialTimepickerModule,
+  ],
   templateUrl: './schedule-admin.component.html',
   styleUrl: './schedule-admin.component.scss',
+  providers: [DatePipe],
 })
 export class ScheduleAdminComponent implements OnInit {
   schedules: Schedule[] = [];
@@ -29,10 +35,7 @@ export class ScheduleAdminComponent implements OnInit {
   openPmTime: string | null = null;
   closePmTime: string | null = null;
 
-  constructor(
-    private fb: FormBuilder,
-    private scheduleService: ScheduleService //private datePipe: DatePipe
-  ) {
+  constructor(private fb: FormBuilder, private scheduleService: ScheduleService) {
     this.scheduleForm = this.fb.group(
       {
         openAmTime: new FormControl(''),
@@ -51,18 +54,80 @@ export class ScheduleAdminComponent implements OnInit {
   }
 
   editSchedule(dayId: number) {
-    console.log(dayId);
-
     this.editingSchedule = this.schedules.find((schedule) => schedule.dayId === dayId) || null;
-
-    /* this.editingSchedule = this.schedules.find((schedule) => schedule.dayId === dayId) || null;
     if (this.editingSchedule) {
       this.scheduleForm.patchValue({
-        openAmTime: this.editingSchedule.openAm,
-        closeAmTime: this.editingSchedule.closeAm,
-        openPmTime: this.editingSchedule.openPm,
-        closePmTime: this.editingSchedule.closePm,
+        openAmTime: this.popSeconds(this.editingSchedule.openAm),
+        closeAmTime: this.popSeconds(this.editingSchedule.closeAm),
+        openPmTime: this.popSeconds(this.editingSchedule.openPm),
+        closePmTime: this.popSeconds(this.editingSchedule.closePm),
       });
-    } */
+    }
+  }
+
+  onEditSchedule() {
+    const group = this.scheduleForm;
+    let data = this.editingSchedule;
+
+    if (group.invalid) {
+      return alert(`Erreur: ${group.errors?.['timeOrder']}`);
+    }
+
+    if (data) {
+      data.openAm = this.formatTime(group.get('openAmTime')?.value);
+      data.closeAm = this.formatTime(group.get('closeAmTime')?.value);
+      data.openPm = this.formatTime(group.get('openPmTime')?.value);
+      data.closePm = this.formatTime(group.get('closePmTime')?.value);
+
+      this.scheduleService.updateSchedule(data.dayId, data).subscribe({
+        next: () => {
+          this.editingSchedule = null;
+          alert('Horaires modifiées');
+        },
+        error: (err) => {
+          console.error('Erreur lors de la mise à jour des horaires', err);
+          alert('Erreur lors de la mise à jour des horaires');
+        },
+      });
+    }
+  }
+
+  private formatTime(time: string | null): string | null {
+    if (time === null || time === '') {
+      return null;
+    }
+    if (!/^\d{2}:\d{2}$/.test(time)) {
+      console.error('Invalid time format');
+      return null;
+    }
+    return `${time}:00`;
+  }
+
+  onCancelEdit() {
+    this.editingSchedule = null;
+  }
+
+  onResetOpenAmTime() {
+    this.scheduleForm.patchValue({ openAmTime: null });
+  }
+
+  onResetCloseAmTime() {
+    this.scheduleForm.patchValue({ closeAmTime: null });
+  }
+
+  onResetOpenPmTime() {
+    this.scheduleForm.patchValue({ openPmTime: null });
+  }
+
+  onResetClosePmTime() {
+    this.scheduleForm.patchValue({ closePmTime: null });
+  }
+
+  popSeconds(timeString: string | null): string {
+    if (!timeString) {
+      return '';
+    }
+    const time = timeString.slice(0, -3);
+    return time;
   }
 }
--- client/src/app/validators/custom.validators.ts
+++ client/src/app/validators/custom.validators.ts
@@ -4,10 +4,10 @@ export class CustomValidators {
   static timeOrderValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       const group = control as FormGroup;
-      const openAmTime = group.get('openAmTime')?.value;
-      const closeAmTime = group.get('closeAmTime')?.value;
-      const openPmTime = group.get('openPmTime')?.value;
-      const closePmTime = group.get('closePmTime')?.value;
+      const openAmTime = convertToDate(group.get('openAmTime')?.value);
+      const closeAmTime = convertToDate(group.get('closeAmTime')?.value);
+      const openPmTime = convertToDate(group.get('openPmTime')?.value);
+      const closePmTime = convertToDate(group.get('closePmTime')?.value);
 
       const isValid: boolean =
         (!openPmTime &&
@@ -20,14 +20,30 @@ export class CustomValidators {
           openPmTime !== null &&
           closePmTime !== null &&
           openPmTime < closePmTime) ||
-        (openAmTime < closeAmTime && closeAmTime < openPmTime && openPmTime < closePmTime) ||
         (openAmTime !== null &&
-          !closeAmTime &&
-          !openPmTime &&
+          closeAmTime !== null &&
+          openPmTime !== null &&
+          closePmTime !== null &&
+          openAmTime < closeAmTime &&
+          closeAmTime < openPmTime &&
+          openPmTime < closePmTime) ||
+        (openAmTime !== null &&
+          closeAmTime === null &&
+          openPmTime === null &&
           closePmTime !== null &&
           openAmTime < closePmTime);
 
-      return isValid ? null : { timeOrder: true };
+      return isValid ? null : { timeOrder: "L'enchaînement des horaires n'est pas conforme" };
     };
   }
+}
+
+function convertToDate(timeString: string | null): Date | null {
+  if (!timeString) {
+    return null;
+  }
+  const [hours, minutes] = timeString.split(':').map(Number);
+  const time = new Date();
+  time.setHours(hours, minutes || 0, 0);
+  return time;
 }
