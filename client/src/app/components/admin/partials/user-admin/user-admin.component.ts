d38f4e14488ec0fd79a2319b278d46cc0d98a567
Thomas P <thomas.philipps@gmail.com>
2024-05-16 22:55:08 +0200

    Implementing user admin files

---
+++ client/src/app/components/admin/partials/user-admin/user-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arz-user-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-admin.component.html',
+  styleUrl: './user-admin.component.scss'
+})
+export class UserAdminComponent {
+
+}
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { SqlGlobalService } from './sql-global.service';
+import { User } from '@app/interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment.development';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService extends SqlGlobalService<User> {
+  constructor(http: HttpClient) {
+    super(http, environment.apiURL + '/users');
+  }
+}
