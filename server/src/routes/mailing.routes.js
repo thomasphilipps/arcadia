0f49e3599eea969fe74cdeaf1edb9aef1577a997
Thomas P <thomas.philipps@gmail.com>
2024-05-17 16:18:58 +0200

    Added environment variables to mailing route

--- server/src/routes/mailing.routes.js
+++ server/src/routes/mailing.routes.js
@@ -2,27 +2,16 @@ const nodemailer = require('nodemailer');
 
 module.exports = (app) => {
   let transporter = null;
-  if (process.env.NODE_ENV === 'prod') {
-    transporter = nodemailer.createTransport({
-      host: 'your host',
-      port: 'your port',
-      secure: false,
-      auth: {
-        user: 'the user of smtp',
-        password: 'the password',
-      },
-    });
-  } else {
-    transporter = nodemailer.createTransport({
-      host: 'localhost',
-      port: 1025,
-      secure: false,
-      tls: {
-        // do not fail on invalid certs
-        rejectUnauthorized: false,
-      },
-    });
-  }
+
+  transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: process.env.SMTP_SECURE === 'true',
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
   app.post('/api/send-email', (req, res) => {
     const { to, subject, text, html } = req.body;
