822d26fd1e6fb425031ce9f3caea44a19c3d266f
thomasphilipps <thomas.philipps@gmail.com>
2024-05-26 22:30:16 +0200

    Added permission for API routes used until now

--- server/src/routes/animal.routes.js
+++ server/src/routes/animal.routes.js
@@ -3,9 +3,9 @@ const { sequelize } = require('../config/database');
 const crud = require('./crud')(sequelize, 'Animal', 'Animals');
 
 module.exports = (app) => {
-  app.post('/api/animals/', crud.create);
+  app.post('/api/animals/', authenticate(['ROLE_ADMIN']), crud.create);
   app.get('/api/animals', crud.readAll);
   app.get('/api/animals/:id', crud.readById);
-  app.put('/api/animals/:id', crud.update);
-  app.delete('/api/animals/:id', crud.delete);
+  app.put('/api/animals/:id', authenticate(['ROLE_ADMIN']), crud.update);
+  app.delete('/api/animals/:id', authenticate(['ROLE_ADMIN']), crud.delete);
 };
