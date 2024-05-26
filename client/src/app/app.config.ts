8c0bc801b738eeac1f84fd46552030d5efa0cf63
thomasphilipps <thomas.philipps@gmail.com>
2024-05-27 01:54:26 +0200

    Update HttpClient configuration with auth interceptors

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,14 +2,26 @@ import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './routes/app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { environment } from '@environments/environment.development';
+import { AuthInterceptor } from './middlewares/interceptor.service';
 export const API_URL = new InjectionToken<string>('apiURL');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
     provideAnimationsAsync(),
     { provide: API_URL, useValue: environment.apiURL },
   ],
