44d06ac421afc362ccbbddddd4bcc3c201b51c2e
thomasphilipps <thomas.philipps@gmail.com>
2024-05-27 02:31:11 +0200

    Replace withInterceptorsFromDi by withInterceptors  as support for DI-provided interceptors may be phased out in a later Angular release.

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -2,26 +2,15 @@ import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './routes/app.routes';
-import {
-  HTTP_INTERCEPTORS,
-  provideHttpClient,
-  withFetch,
-  withInterceptors,
-  withInterceptorsFromDi,
-} from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { environment } from '@environments/environment.development';
-import { AuthInterceptor } from './middlewares/interceptor.service';
+import { AuthInterceptor } from './middlewares/interceptor';
 export const API_URL = new InjectionToken<string>('apiURL');
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptorsFromDi()),
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true,
-    },
+    provideHttpClient(withFetch(), withInterceptors([AuthInterceptor])),
     provideAnimationsAsync(),
     { provide: API_URL, useValue: environment.apiURL },
   ],
