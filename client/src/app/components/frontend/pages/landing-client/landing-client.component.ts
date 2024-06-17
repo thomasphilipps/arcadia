4b61ae41d43ae608e9542acdcf1ad638232e2d59
thomasphilipps <thomas.philipps@gmail.com>
2024-06-17 11:45:02 +0200

    [WIP] Styling client

--- client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
+++ client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'arz-landing-client',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './landing-client.component.html',
   styleUrl: './landing-client.component.scss',
 })
