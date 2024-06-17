9efee09b142ac0a7815630c877fdb9351a46782c
thomasphilipps <thomas.philipps@gmail.com>
2024-06-17 10:35:38 +0200

    Begin theming

--- client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
+++ client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'arz-landing-client',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   templateUrl: './landing-client.component.html',
   styleUrl: './landing-client.component.scss',
 })
