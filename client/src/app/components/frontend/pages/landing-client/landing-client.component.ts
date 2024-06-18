cbcf8f53da345412ef68c7280a4a4189caa360af
Thomas P <thomas.philipps@gmail.com>
2024-06-18 17:00:12 +0200

    Schedule in footer

--- client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
+++ client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
@@ -3,11 +3,19 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../partials/footer/footer.component';
 
 @Component({
   selector: 'arz-landing-client',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, RouterLink],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterLink,
+    FooterComponent,
+  ],
   templateUrl: './landing-client.component.html',
   styleUrl: './landing-client.component.scss',
 })
