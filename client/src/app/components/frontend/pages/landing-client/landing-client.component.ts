b350f77e1a89a807e4b4679aeff0fbcc6e687cf9
thomasphilipps <thomas.philipps@gmail.com>
2024-06-20 21:24:54 +0200

    chore: Update landing page navigation tooltips and button labels

--- client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
+++ client/src/app/components/frontend/pages/landing-client/landing-client.component.ts
@@ -4,12 +4,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../../partials/footer/footer.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'arz-landing-client',
   standalone: true,
   imports: [
     RouterOutlet,
+    MatTooltipModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
