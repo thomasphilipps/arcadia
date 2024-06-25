45a69805c4541e743be0db6112a8f7b213a915d2
thomasphilipps <thomas.philipps@gmail.com>
2024-06-25 15:58:05 +0200

    chore: Update service client component with new layout and data loading

--- client/src/app/components/frontend/partials/service-client/service-client.component.ts
+++ client/src/app/components/frontend/partials/service-client/service-client.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Service } from '@app/interfaces/service.interface';
+import { ServiceService } from '@app/services/service.service';
 
 @Component({
   selector: 'arz-service-client',
   standalone: true,
   imports: [],
   templateUrl: './service-client.component.html',
-  styleUrl: './service-client.component.scss'
+  styleUrl: './service-client.component.scss',
 })
-export class ServiceClientComponent {
+export class ServiceClientComponent implements OnInit {
+  services: Service[] = [];
 
+  constructor(private serviceService: ServiceService) {}
+
+  ngOnInit(): void {
+    this.loadServices();
+  }
+
+  loadServices(): void {
+    this.serviceService.loadData();
+    this.serviceService.getAllData().subscribe({
+      next: (services) => {
+        this.services = services;
+      },
+      error: (error) => {
+        console.error('Erreur lors du chargement des données:', error);
+      },
+    });
+  }
 }
