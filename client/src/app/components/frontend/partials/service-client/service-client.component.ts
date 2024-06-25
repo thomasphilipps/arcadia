220228959b9602817448051a981bbc1a02e3fcde
thomasphilipps <thomas.philipps@gmail.com>
2024-06-25 09:59:28 +0200

    Initialize services page

---
+++ client/src/app/components/frontend/partials/service-client/service-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arz-service-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './service-client.component.html',
+  styleUrl: './service-client.component.scss'
+})
+export class ServiceClientComponent {
+
+}
