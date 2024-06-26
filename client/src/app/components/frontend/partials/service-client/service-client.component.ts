39aa3e3fdd700a16958c3f8139ac41c75ee448b4
thomasphilipps <thomas.philipps@gmail.com>
2024-06-26 14:56:46 +0200

    Services page with images

--- client/src/app/components/frontend/partials/service-client/service-client.component.ts
+++ client/src/app/components/frontend/partials/service-client/service-client.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 import { Service } from '@app/interfaces/service.interface';
+import { ImageService } from '@app/services/image.service';
 import { ServiceService } from '@app/services/service.service';
 
 @Component({
   selector: 'arz-service-client',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './service-client.component.html',
   styleUrl: './service-client.component.scss',
 })
 export class ServiceClientComponent implements OnInit {
   services: Service[] = [];
 
-  constructor(private serviceService: ServiceService) {}
+  constructor(private serviceService: ServiceService, private imageService: ImageService) {}
 
   ngOnInit(): void {
     this.loadServices();
@@ -23,10 +26,26 @@ export class ServiceClientComponent implements OnInit {
     this.serviceService.getAllData().subscribe({
       next: (services) => {
         this.services = services;
+        this.loadImages();
       },
       error: (error) => {
         console.error('Erreur lors du chargement des données:', error);
       },
+    });
+  }
+
+  loadImages(): void {
+    this.services.forEach((service) => {
+      this.imageService
+        .getImageByReferenceTypeAndId('Service', service.serviceId.toString())
+        .subscribe({
+          next: (images) => {
+            service.images = images;
+          },
+          error: (error) => {
+            console.error('Erreur lors du chargement des images:', error);
+          },
+        });
     });
   }
 }
