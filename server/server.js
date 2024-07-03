55e7eff0e1ede0168b82cfa9724d194f1de9f339
thomasphilipps <thomas.philipps@gmail.com>
2024-07-03 19:22:13 +0200

    Modified environment vars for Heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const app = require("./src/config/serverConfig");
+const app = require('./src/config/serverConfig');
 
-const api_port = process.env.API_PORT;
-const api_host = process.env.API_HOST;
+const api_port = process.env.PORT;
+const api_host = process.env.HOST;
 
 app.listen(api_port, () => console.log(`Node server started on : ${api_host}:${api_port}`));
