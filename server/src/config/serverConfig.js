19456f0e568ba9be3e409c5e7217fc0f484491e9
thomasphilipps <thomas.philipps@gmail.com>
2024-07-22 16:03:04 +0200

    Revert "less cors policy"
    This reverts commit 02aea20982503f31d60e97014719f7a3bde7b51d.

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -28,7 +28,7 @@ const corsOptions = {
 const app = express();
 
 // Middlewares
-app.use(cors()).use(bodyParser.json()).use(apiLimiter);
+app.use(cors(corsOptions)).use(bodyParser.json()).use(apiLimiter);
 
 // Main route to test the server
 app.get('/', (req, res) => {
