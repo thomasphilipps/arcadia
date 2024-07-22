da5bf1fca5dd5a00e3451b476a8c20eb4c979c64
Thomas P <thomas.philipps@gmail.com>
2024-07-22 11:03:18 +0200

    chore: Update mailing routes to support OAuth2 authentication

--- server/src/routes/mailing.routes.js
+++ server/src/routes/mailing.routes.js
@@ -1,18 +1,44 @@
 const { authenticate } = require('../middlewares/auth');
 const nodemailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
 module.exports = (app) => {
   let transporter = null;
 
-  transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE === 'true',
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  if (process.env.NODE_ENV === 'production') {
+    const myOAuth2Client = new OAuth2(
+      process.env.OAUTH_ID,
+      process.env.OAUTH_SECRET,
+      'https://developers.google.com/oauthplayground'
+    );
+
+    myOAuth2Client.setCredentials({
+      refresh_token: process.env.OAUTH_REFRESH_TOKEN,
+    });
+
+    transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.SMTP_USER,
+        clientId: process.env.OAUTH_ID,
+        clientSecret: process.env.OAUTH_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken: myOAuth2Client.getAccessToken(),
+      },
+    });
+  } else {
+    transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: process.env.SMTP_SECURE === 'true',
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
 
   app.post('/api/send-email', (req, res) => {
     const { from, to, subject, text, html } = req.body;
